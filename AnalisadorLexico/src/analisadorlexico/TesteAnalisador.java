49115caed3610b5c9c3e31538187a0ebb7b702d1
elyabe <elly@elly-Lenovo-G485>
2017-06-25 15:46:05 -0300

    Inclusao das novas de classes do analisador; Problema com loop na classe principal

--- AnalisadorLexico/src/analisadorlexico/TesteAnalisador.java
+++ AnalisadorLexico/src/analisadorlexico/TesteAnalisador.java
@@ -1,8 +1,35 @@
 
 package analisadorlexico;
 
-public class TesteAnalisador {
+public class TesteAnalisador 
+{
+    static public AnalisadorEstados scanner;
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            // TEMPORARIA: Modificaçao para nao ter que passar nome do arquivo pelo terminal
+            if ( args.length != 1 )
+            {
+//                throw new RuntimeException("Nome do arquivo obrigatorio");
+                scanner = new AnalisadorEstados( "entrada.txt" );
+            }  else
+                scanner = new AnalisadorEstados( args[0] );    
+            
+            do
+            {
+                scanner.q0();
+                System.out.println( scanner.tokenReconhecido );
+            } while( scanner.tokenReconhecido != Constantes.Token.EOF);
+                    
+            System.out.println("Sucesso! :D");
+            
+        } catch (ErroLexico e) {
+            System.out.println("Erro lexico: " + e.toString() );
+        } catch (RuntimeException e) {
+            System.out.println("Erro lexico2: " + e.getMessage() );
+        }
 
-    public static void main(String[] args) {
+        
     }
 }
